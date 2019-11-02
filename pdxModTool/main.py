86285dd72100a210017ad7c939665bc329f9f1d9
Arash Ma <arashm35@gmail.com>
2019-11-02 16:12:30 +0100

    self update

--- pdxModTool/main.py
+++ pdxModTool/main.py
@@ -44,12 +44,15 @@ def recv(args):
 
 
 def update(args):
-    subprocess.check_call(
-        [
-            'pipx', 'upgrade', '--spec',
-            f'git+https://github.com/arashm35/pdxModTool{args.branch}#egg=pdxModTool', 'pdxModTool'
-        ]
-    )
+    try:
+        subprocess.check_call(
+            [
+                'pipx', 'upgrade', '--spec',
+                f'git+https://github.com/arashm35/pdxModTool{args.branch}#egg=pdxModTool', 'pdxModTool'
+            ]
+        )
+    except subprocess.CalledProcessError:
+        return
 
 
 def main():
