bcd2d73e33c5f0e6120ede3edcfc418df2bf1fab
Arash Ma <arashm35@gmail.com>
2019-11-02 04:32:04 +0100

    add send and recv entry points

--- pdxModTool/main.py
+++ pdxModTool/main.py
@@ -1,18 +1,39 @@
-from pdxModTool.cli import parser, parser_build, parser_install
+import logging
+
+from pdxModTool.cli import parser, parser_build, parser_install, parser_send, parser_recv
 from pdxModTool.pdxmod import PDXMod
+from pdxModTool.server import Server
 from pdxModTool.util import get_mod_dir
 
 
-def build(args, mod: PDXMod):
+def build(args):
     output_dir = args.output if args.output else args.path
+    mod = PDXMod()
+    if args.path.exists:
+        mod.read_from_dir(args.path)
+
     print(f'building {mod.modName} to {output_dir}')
     mod.build(output_dir, desc=args.descriptor)
 
 
-def install(args, mod: PDXMod):
+def install(args):
+    mod = PDXMod()
+    if args.path.exists:
+        mod.read_from_dir(args.path)
+
     print(f'install {mod.modName} to {args.game} mod folder')
     mod_dir = get_mod_dir(args.game)
     mod.build(mod_dir, desc=True, backup=args.backup)
+
+
+def send(args):
+    print(args)
+    server = Server(args.ip, args.port)
+    server.start()
+
+
+def recv(args):
+    pass
 
 
 def main():
@@ -20,14 +41,12 @@ def main():
 
     parser_build.set_defaults(func=build)
     parser_install.set_defaults(func=install)
+    parser_send.set_defaults(func=send)
+    parser_recv.set_defaults(func=recv)
 
-    mod = PDXMod()
     args = parser.parse_args()
 
-    if args.path.exists():
-        mod.read_from_dir(args.path)
-
-    args.func(args, mod)
+    args.func(args)
 
 
 if __name__ == '__main__':
